f8434fe66659e2696cb2d12ecf695b65604799d1
andrehideki <andre.hkiw.dev@gmail.com>
2022-05-25 16:11:31 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const {defaults} = require("jest-config");
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts'],
+testMatch: [ "<rootDir>/src/__tests__/**/*.spec.ts" ]
+};
